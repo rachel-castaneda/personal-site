f293010f39380b099ef9dc7a0d6dec3412f888dc
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-15 14:54:25 -0600

    worked on about me toggle

--- js/personal-js.js
+++ js/personal-js.js
@@ -1,2 +1,16 @@
 "use strict"
 
+$(document).ready(function () {
+
+    // $(".about-me").hover(function () {
+    //     $(this).css('visibility', 'hidden');
+    // }, function (){
+    //     $(this).css('visibility', 'visible');
+    // })
+
+    $('.toggle').click(function () {
+        $('.about-me').toggle();
+    });
+
+
+})
